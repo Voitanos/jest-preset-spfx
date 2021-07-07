d287fd0fce596e2f1d71aa3d67c90485df89becb
Andrew Connell <me@andrewconnell.com>
2021-07-07 15:32:04 -0400

    (workflow) update publish prerelease version

--- scripts/update-package-version.js
+++ scripts/update-package-version.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const packageJson = require('../package.json');
-packageJson.version += `-beta.${process.argv[process.argv.length-1].substr(0, 7)}`;
-console.log(packageJson.version);
-fs.writeFileSync(path.join(path.resolve('.'), 'package.json'), JSON.stringify(packageJson, null, 2));
+const release_tag = `${process.argv[process.argv.length-2]}`;
+const release_name = `${process.argv[process.argv.length-1]}`;
+packageJson.version += `-${release_name.replace(release_tag + '-', '')}`;
+console.log('publish version:', packageJson.version);
+// fs.writeFileSync(path.join(path.resolve('.'), 'package.json'), JSON.stringify(packageJson, null, 2));
